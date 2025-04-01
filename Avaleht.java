22f78cef0e21284c887e15e6eb6cf3ce2eb841e2
GloriaV03 <veskimei@ut.ee>
2025-04-01 12:17:39 +0300

    Update Avaleht.java

--- Avaleht.java
+++ Avaleht.java
@@ -1,7 +1,15 @@
 package ee.oop.retseptid.scraper;
 
 public class Avaleht {
-    private static String üldInfo = "";
+    private static String üldInfo = "Tere tulemast retseptiotsija programmi!\n" +
+            "\n" +
+            "See programm aitab sul leida maitsvaid retsepte, kui annad ette ühe toiduaine.\n" +
+            "Näiteks kui sul on kodus \"kana\", saad teada, milliseid roogi võiksid sellega valmistada.\n" +
+            "\n" +
+            "Sisesta lihtsalt üks koostisosa (näiteks: kartul, porgand, õun) ja saad nimekirja retseptidest,\n" +
+            "milles seda koostisosa kasutatakse.\n" +
+            "\n" +
+            "Alustamiseks sisesta koostisosa ja vajuta Enter. ";
 
     public static String getÜldInfo() {
         return üldInfo;
